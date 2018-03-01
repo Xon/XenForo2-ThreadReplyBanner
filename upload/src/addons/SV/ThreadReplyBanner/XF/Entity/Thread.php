c8c9e7cb8d3c19618c96e1369a2fb77342821975
Xon <tazipper1@hotmail.com>
2018-03-02 00:09:32 +0800

    Cleanup of thread entity

--- upload/src/addons/SV/ThreadReplyBanner/XF/Entity/Thread.php
+++ upload/src/addons/SV/ThreadReplyBanner/XF/Entity/Thread.php
@@ -4,7 +4,7 @@ namespace SV\ThreadReplyBanner\XF\Entity;
 
 use XF\Mvc\Entity\Structure;
 
-/*
+/**
  * Extends \XF\Entity\Thread
  *
  * COLUMNS
@@ -21,43 +21,63 @@ class Thread extends XFCP_Thread
      */
     public function canManageThreadReplyBanner(/** @noinspection PhpUnusedParameterInspection */&$error = null)
     {
-        return \XF::visitor()->hasNodePermission(
-            $this->node_id,
-            'sv_replybanner_manage'
-        );
+        return \XF::visitor()->hasNodePermission($this->node_id, 'sv_replybanner_manage');
     }
 
-    public function canViewBanner()
+    /**
+     * @param string|null $error
+     * @return bool
+     */
+    public function canViewBanner(/** @noinspection PhpUnusedParameterInspection */&$error = null)
     {
-        return (
-                   \XF::visitor()->hasPermission('forum', 'sv_replybanner_show') ||
-                   \XF::visitor()->hasPermission('forum', 'sv_replybanner_manage')
-               ) && $this->ThreadBanner && $this->ThreadBanner->banner_state;
+        if (!$this->has_banner)
+        {
+            return false;
+        }
+        $visitor = \XF::visitor();
+
+        if (!$visitor->hasPermission('forum', 'sv_replybanner_show') &&
+            !$visitor->hasPermission('forum', 'sv_replybanner_manage'))
+        {
+            return false;
+        }
+
+        return $this->ThreadBanner && $this->ThreadBanner->banner_state;
     }
 
+    /**
+     * @return array
+     */
     public function getThreadBanner()
     {
-        if ($this->has_banner)
+        $threadBanner = $this->ThreadBanner;
+        if (!$threadBanner || !$threadBanner->banner_state)
         {
-            return [
-                'title'            => '',
-                'message'          => $this->ThreadBanner->getRenderedBannerText(),
-                'active'           => true,
-                'display_order'    => 1,
-                'dismissible'      => false,
-                'user_criteria'    => [],
-                'page_criteria'    => [],
-                'notice_type'      => 'scrolling',
-                'display_style'    => 'primary',
-                'css_class'        => '',
-                'display_duration' => 0,
-                'delay_duration'   => 0,
-                'auto_dismiss'     => 0
-            ];
+            return [];
         }
-        else
+        return [
+            'title'            => '',
+            'message'          => $threadBanner->getRenderedBannerText(),
+            'active'           => true,
+            'display_order'    => 1,
+            'dismissible'      => false,
+            'user_criteria'    => [],
+            'page_criteria'    => [],
+            'notice_type'      => 'scrolling',
+            'display_style'    => 'primary',
+            'css_class'        => '',
+            'display_duration' => 0,
+            'delay_duration'   => 0,
+            'auto_dismiss'     => 0
+        ];
+    }
+
+    protected function _postDelete()
+    {
+        parent::_postDelete();
+        if ($this->ThreadBanner)
         {
-            return null;
+            $this->ThreadBanner->delete();
         }
     }
 
