3b9662c1e8c4243996581853233bfd393e4dd4aa
Fillip H. <9424286+filliph@users.noreply.github.com>
2024-11-18 17:22:12 +0200

    Convert JS to vanillaJS

--- upload/src/addons/SV/ThreadReplyBanner/_files/js/sv/threadreplybanner/scroll-to.js
+++ upload/src/addons/SV/ThreadReplyBanner/_files/js/sv/threadreplybanner/scroll-to.js
@@ -1,22 +1,43 @@
-$(document).ready(function()
+var SV = window.SV || {};
+SV.$ = SV.$ || window.jQuery || null;
+
+((window, document) =>
 {
-    var focusEditor = XF.focusEditor;
-    XF.focusEditor = function ($container, notConstraints)
+    'use strict';
+    var $ = SV.$, xf22 = typeof XF.on !== "function";
+
+    let focusEditor = XF.focusEditor;
+
+    /**
+     * @param {HTMLElement|jQuery} $container
+     * @param {boolean} notConstraints
+     */
+    XF.focusEditor = ($container, notConstraints) =>
     {
-        var $el = $container.closest('.js-quickReply');
-        if ($el.length > 0)
+        /** @type HTMLElement */
+        const container = xf22 ? $container.get(0) : $container;
+
+        const el = container.closest('.js-quickReply');
+        if (el)
         {
-            var height = $(".p-navSticky.is-sticky").height();
-            if (height > 0)
+            const stickyNav = document.querySelector('.p-navSticky.is-sticky');
+            if (stickyNav)
             {
-                var viewportOffset = $el.get(0).getBoundingClientRect();
-                if (viewportOffset.top < height)
+                let height = stickyNav
+                    .getBoundingClientRect()
+                    .height
+                ;
+                if (height > 0)
                 {
-                    window.scroll(0, window.scrollY - height + viewportOffset.top - 1);
+                    const viewportOffset = el.getBoundingClientRect();
+                    if (viewportOffset.top < height)
+                    {
+                        window.scroll(0, window.scrollY - height + viewportOffset.top - 1);
+                    }
+                    return;
                 }
-                return;
             }
         }
         focusEditor($container, notConstraints);
     };
-});
+})(window, document)
