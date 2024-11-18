7b8e75fbfbabe95f3ac13512556aa48875489dda
Fillip H. <9424286+filliph@users.noreply.github.com>
2024-11-18 17:04:40 +0200

    Update exports

---
+++ upload/src/addons/SV/ThreadReplyBanner/_output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property non-negative-int|null $svThreadReplyBanner_showBanner Show thread reply banner in
+ */
+class Options
+{
+}
